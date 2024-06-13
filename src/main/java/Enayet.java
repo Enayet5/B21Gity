c020c021f8ca00d3f14678595c2d7d2de6bf6a2f
Enayet Ullah1 <enayetullah1@Enayets-MacBook-Pro.local>
2024-06-12 20:46:02 -0500

    stashing message

--- src/main/java/Enayet.java
+++ src/main/java/Enayet.java
@@ -3,5 +3,6 @@ public class Enayet {
     public static  void main (){
         System.out.println("a massage from git");
         System.out.println("Git add after  git");
+        System.out.println("Message from Stash");
     }
 }
